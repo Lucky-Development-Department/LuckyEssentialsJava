d20fb91c58bbe2036a8bd13dc22a4ff482f14955
BadAccuracyID <efran.nathanael@gmail.com>
2021-08-12 08:30:09 +0700

    - Improved FlyCommand messages

--- plugin/src/main/java/id/luckynetwork/dev/lyrams/lej/commands/FlyCommand.java
+++ plugin/src/main/java/id/luckynetwork/dev/lyrams/lej/commands/FlyCommand.java
@@ -75,7 +75,7 @@ public class FlyCommand extends CommandClass {
 
         if (others) {
             sender.sendMessage(Config.PREFIX + "§eToggled flight for §d" + targets.size() + " §eplayers!");
-        } else {
+        } else if (sender instanceof Player && !targets.contains((Player) sender)) {
             targets.stream().findFirst().ifPresent(target -> sender.sendMessage(Config.PREFIX + "§eFlight mode for §d" + target.getName() + "§e: " + Utils.colorizeTrueFalse(target.getAllowFlight(), TrueFalseType.ON_OFF) + "§e!"));
         }
     }
