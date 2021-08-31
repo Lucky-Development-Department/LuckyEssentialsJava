801a58b3577dd4a7aca5cebed9cf5d910915ebab
BadAccuracyID <efran.nathanael@gmail.com>
2021-08-31 21:01:42 +0700

    - Improved SmiteCommand

--- plugin/src/main/java/id/luckynetwork/dev/lyrams/lej/commands/essentials/SmiteCommand.java
+++ plugin/src/main/java/id/luckynetwork/dev/lyrams/lej/commands/essentials/SmiteCommand.java
@@ -3,12 +3,14 @@ package id.luckynetwork.dev.lyrams.lej.commands.essentials;
 import cloud.commandframework.annotations.Argument;
 import cloud.commandframework.annotations.CommandDescription;
 import cloud.commandframework.annotations.CommandMethod;
+import cloud.commandframework.annotations.Flag;
 import id.luckynetwork.dev.lyrams.lej.commands.api.CommandClass;
 import id.luckynetwork.dev.lyrams.lej.utils.Utils;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -20,7 +22,8 @@ public class SmiteCommand extends CommandClass {
     @CommandDescription("Summons lighting where you're looking at or at other player")
     public void smiteCommand(
             final @NonNull CommandSender sender,
-            final @NonNull @Argument(value = "target", description = "The target player", defaultValue = "self", suggestions = "players") String targetName
+            final @NonNull @Argument(value = "target", description = "The target player", defaultValue = "self", suggestions = "players") String targetName,
+            final @Nullable @Flag(value = "silent", aliases = "s", description = "Should the target not get notified?") Boolean silent
     ) {
         if (!Utils.checkPermission(sender, "smite")) {
             return;
@@ -36,7 +39,9 @@ public class SmiteCommand extends CommandClass {
 
         if (!targets.isEmpty()) {
             locations.addAll(targets.stream().map(Player::getLocation).collect(Collectors.toList()));
-            targets.forEach(target -> target.sendMessage(plugin.getMainConfigManager().getPrefix() + "§eYou have been smitten!"));
+            if (silent == null || !silent) {
+                targets.forEach(target -> target.sendMessage(plugin.getMainConfigManager().getPrefix() + "§eYou have been smitten!"));
+            }
         }
 
         locations.forEach(location -> location.getWorld().strikeLightning(location));
