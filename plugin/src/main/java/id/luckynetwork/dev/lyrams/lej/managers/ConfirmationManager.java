fd054c3f7e2ec52769e65733b7d2a9273c508079
BadAccuracyID <efran.nathanael@gmail.com>
2021-11-06 01:29:54 +0700

    - Added JavaDocs in ConfirmationManager

--- plugin/src/main/java/id/luckynetwork/dev/lyrams/lej/managers/ConfirmationManager.java
+++ plugin/src/main/java/id/luckynetwork/dev/lyrams/lej/managers/ConfirmationManager.java
@@ -17,6 +17,14 @@ public class ConfirmationManager {
     private final LuckyEssentials plugin;
     private final Map<Player, Callable> confirmationMap = new HashMap<>();
 
+    /**
+     * Requests a confirmation before executing something
+     *
+     * @param callable the code to execute if confirmed
+     * @param sender   the executor
+     * @param skip     the required condition to skip the confirmation
+     * @param warnings warnings to be sent to the player
+     */
     public void requestConfirmation(Callable callable, CommandSender sender, boolean skip, @Nullable List<String> warnings) {
         if (skip || sender instanceof ConsoleCommandSender) {
             callable.call();
@@ -32,26 +40,47 @@ public class ConfirmationManager {
         this.confirmationMap.put(player, callable);
     }
 
+    /**
+     * @see ConfirmationManager#requestConfirmation(Callable, CommandSender, boolean, List)
+     */
     public void requestConfirmation(Callable callable, CommandSender sender, boolean skip) {
         this.requestConfirmation(callable, sender, skip, null);
     }
 
+    /**
+     * @see ConfirmationManager#requestConfirmation(Callable, CommandSender, boolean, List)
+     */
     public void requestConfirmation(Callable callable, CommandSender sender, @Nullable List<String> warnings) {
         this.requestConfirmation(callable, sender, false, warnings);
     }
 
+    /**
+     * @see ConfirmationManager#requestConfirmation(Callable, CommandSender, boolean, List)
+     */
     public void requestConfirmation(Callable callable, CommandSender sender) {
         this.requestConfirmation(callable, sender, false, null);
     }
 
+    /**
+     * Confirms the execution of a pending code
+     *
+     * @param player the player
+     */
     public void confirm(Player player) {
         if (!this.confirmationMap.containsKey(player)) {
             return;
         }
 
+        player.sendMessage(plugin.getMainConfigManager().getPrefix() + "§aAction confirmed.");
         this.confirmationMap.get(player).call();
     }
 
+
+    /**
+     * Deletes the queued pending code from {@link ConfirmationManager#confirmationMap}
+     *
+     * @param player the player who has the code pending
+     */
     public void deleteConfirmation(Player player) {
         this.confirmationMap.remove(player);
     }
