90b2c12a3193ee7e4921a94ed8f868c34d96f4d3
BadAccuracyID <37508513+BadAccuracyID@users.noreply.github.com>
2022-07-09 20:27:52 +0700

    Moved ExplodeCommand#explodeCommand to TrollCommands#explodeCommand

--- plugin/src/main/java/id/luckynetwork/dev/lyrams/lej/commands/trolls/TrollCommands.java
+++ plugin/src/main/java/id/luckynetwork/dev/lyrams/lej/commands/trolls/TrollCommands.java
@@ -16,6 +16,10 @@ import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class TrollCommands extends CommandClass {
 
     @ProxiedBy("fakebreak")
@@ -137,6 +141,48 @@ public class TrollCommands extends CommandClass {
             final @NonNull @Argument(value = "toggle", description = "on/off/toggle", defaultValue = "toggle", suggestions = "toggles") String toggle
     ) {
         this.toggleTroll(sender, targetName, toggle, TrollType.NO_BREAK);
+    }
+
+    @ProxiedBy("explode")
+    @CommandMethod("luckytrolls|luckytroll|trolls|troll|lt explode [target] [power] [damage]")
+    @CommandDescription("Spawns an explosion on where you're looking at or at other player")
+    public void explodeCommand(
+            final @NonNull CommandSender sender,
+            final @NonNull @Argument(value = "target", description = "The target player", defaultValue = "self", suggestions = "players") String targetName,
+            final @NonNull @Argument(value = "power", description = "The explosion power", defaultValue = "4") Float power,
+            final @NonNull @Argument(value = "damage", description = "Should the explosion damage blocks", defaultValue = "true") Boolean damage,
+            final @Nullable @Flag(value = "silent", aliases = "s", description = "Should the target not be notified?") Boolean silent
+    ) {
+        if (!Utils.checkPermission(sender, "trolls.explode")) {
+            return;
+        }
+
+        Set<Location> locations = new HashSet<>();
+        TargetsCallback targets = new TargetsCallback();
+        if (targetName.equals("self") && sender instanceof Player) {
+            locations.add(((Player) sender).getTargetBlock(null, 120).getLocation());
+        } else {
+            targets = this.getTargets(sender, targetName);
+        }
+
+        if (!targets.isEmpty()) {
+            locations.addAll(targets.stream().map(Player::getLocation).collect(Collectors.toList()));
+        }
+
+        TargetsCallback finalTargets = targets;
+        plugin.getConfirmationManager().requestConfirmation(() -> {
+            locations.forEach(location -> location.getWorld().createExplosion(location, power, damage));
+            if (silent == null || !silent) {
+                finalTargets.forEach(target -> target.sendMessage(plugin.getMainConfigManager().getPrefix() + "§eYou have been exploded!"));
+            }
+
+            boolean others = !finalTargets.isEmpty() && finalTargets.size() > 1;
+            if (others) {
+                sender.sendMessage(plugin.getMainConfigManager().getPrefix() + "§eExploded §d" + finalTargets.size() + " §eplayers.");
+            } else if ((!(sender instanceof Player)) || (finalTargets.doesNotContain((Player) sender) && !targetName.equals("self"))) {
+                finalTargets.stream().findFirst().ifPresent(target -> sender.sendMessage(plugin.getMainConfigManager().getPrefix() + "§eExploded §d" + target.getName() + "§e."));
+            }
+        }, this.canSkip("troll-Explode", targets, sender));
     }
 
     @ProxiedBy("launch")
