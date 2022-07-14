cc01b94a7b77dafe3608abba4ff8ccd94ae43127
BuildTools <37508513+BadAccuracyID@users.noreply.github.com>
2022-07-14 22:59:42 +0700

    Added TrollCommand#listCommand

--- plugin/src/main/java/id/luckynetwork/dev/lyrams/lej/commands/trolls/TrollCommand.java
+++ plugin/src/main/java/id/luckynetwork/dev/lyrams/lej/commands/trolls/TrollCommand.java
@@ -3,6 +3,7 @@ package id.luckynetwork.dev.lyrams.lej.commands.trolls;
 import cloud.commandframework.annotations.Argument;
 import cloud.commandframework.annotations.CommandDescription;
 import cloud.commandframework.annotations.CommandMethod;
+import cloud.commandframework.annotations.ProxiedBy;
 import id.luckynetwork.dev.lyrams.lej.commands.api.CommandClass;
 import id.luckynetwork.dev.lyrams.lej.enums.TrollType;
 import id.luckynetwork.dev.lyrams.lej.enums.TrueFalseType;
@@ -10,12 +11,12 @@ import id.luckynetwork.dev.lyrams.lej.utils.Utils;
 import org.bukkit.command.CommandSender;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TrollCommand extends CommandClass {
 
-    @CommandMethod("luckytrolls|luckytroll|trolls|troll|lt check [target]")
+    @CommandMethod("luckytrolls|luckytroll|troll|lt check [target]")
     @CommandDescription("Checks active trolls on target")
     public void checkCommand(
             final @NonNull CommandSender sender,
@@ -51,6 +52,32 @@ public class TrollCommand extends CommandClass {
                     sender.sendMessage("§8├─ §e" + key + ": " + value);
                 }
             });
+        });
+    }
+
+    @ProxiedBy("trolls")
+    @CommandMethod("luckytrolls|luckytroll|troll|lt list")
+    @CommandDescription("Lists all available trolls")
+    public void listCommand(
+            final @NonNull CommandSender sender
+    ) {
+        if (!Utils.checkPermission(sender, "trolls")) {
+            return;
+        }
+
+        List<String> trolls = Arrays.stream(TrollType.values()).map(TrollType::getDisplay).collect(Collectors.toCollection(ArrayList::new));
+        trolls.add("Explode");
+        trolls.add("Launch");
+
+        sender.sendMessage("§eAvailable trolls:");
+        final int[] size = {trolls.size()};
+        trolls.forEach((key) -> {
+            boolean last = (--size[0] < 1);
+            if (last) {
+                sender.sendMessage("§8└─ §e" + key);
+            } else {
+                sender.sendMessage("§8├─ §e" + key);
+            }
         });
     }
 
